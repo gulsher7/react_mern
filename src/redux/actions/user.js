d6075ea2b3e2e331df6c9e9d4df9962d184d6235
Gulsher7 <gulsherkhan0005@gmail.com>
2024-02-04 02:48:12 +0530

    feat: create a new action getUserDetailsById

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,10 +1,15 @@
-import { USER_DETAILS } from '../../config/urls';
+import { FETCH_USERS_BY_IDS, USER_DETAILS } from '../../config/urls';
 import { apiGet } from '../../utils/utils';
 
 
 
 export const getUserDetailsById = (query="") => {
   return apiGet(USER_DETAILS+query)
+};
+
+
+export const getUsersByIds = (query="") => {
+  return apiGet(FETCH_USERS_BY_IDS+query)
 };
 
 
